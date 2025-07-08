c5d11d9ea5f9a485bcc6b53782761fa6d523b777
Tharun A M <tharunmgowda1936@gmail.com>
2025-07-08 17:38:24 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,47 @@
+console.log("Hey I am Tharun")
+
+var a=15;
+var b=13;
+var c="Tharun";
+var _d="Nandan";
+console.log(a+b)
+console.log(typeof a,typeof c)
+
+//var 55e is not allowed 
+const a1=6;
+// const a1 = a1 + 1 is not allowed bcz it is constant
+
+{
+    let a = 34
+    console.log(a)         // output is 34   {let is block code} 
+}
+
+console.log(a)       // output is 15
+
+{
+    var a = 44;
+    console.log(a)              // var is global 
+}
+
+console.log(a)
+
+let x = "Tharun";
+let y = 33;
+let z = 22.54;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o ={
+    "name" : "Tharun",
+    Job_role : "Software development Engineer"
+
+}
+
+console.log(o)
+
+o.salary = "10lakh";
+console.log(o)
